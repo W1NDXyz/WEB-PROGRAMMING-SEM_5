ab850f8f3720468569050e4b9c988200e6718542
W1NDX <161310311+W1NDXyz@users.noreply.github.com>
2025-08-20 00:43:47 +0800

    Create 1C_WP_fristCode.php
    Frist Class learning the basic structure of PHP. Showing the echo usage in HTML structure.

---
+++ 1C_WP_fristCode.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Info Student</title>
+		<style>
+			h3{
+				color:skyblue;		
+			}
+			h4{
+				color:green;
+			}
+			b{
+				color:purple;
+			}
+		</style>
+	</head>
+
+	<body>
+		<h3><b>Name:</b> FOO TUN FENG</h3>
+		<h4><b>Department:</b> JTMK</h4>
+		<?php
+		//put your code here
+			echo "<b>Num.Metric:</b> 13DDDT23F1052";
+			echo "<br>";
+			echo "<b>Course:</b> DFP50193  Website Programming";
+			echo "<br>";
+			echo "<b>Lecture:</b> PN. Raja Intan Sariah";
+		?>
+	</body>
+</html>
