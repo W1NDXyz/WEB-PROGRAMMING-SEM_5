98971d441ee6ec6d2ad9d252b95fbd746ba5c9d0
W1NDX <161310311+W1NDXyz@users.noreply.github.com>
2025-08-20 00:45:52 +0800

    Create 2C_WP_variable.php
    Second Class, method to declare variable ($).

---
+++ 2C_WP_variable.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Declare Variable in PHP</title>
+	</head>
+
+	<body>
+
+
+		<?php
+			$name = "FOO";
+			$maticNum = "13DDT23F1052";
+			$dept = "JTMK";
+			$add = "Kuala Terengganu";
+			$notel = "01125223022";
+
+			echo "<h2>Student Information</h2><br>";
+			echo "<b>Name: </b>";
+			echo $name,"<br>";
+			echo "<b>Matic Number: </b>";
+			echo $maticNum, "<br>";
+			echo "<b>Department: </b>";
+			echo $dept,"<br>";
+			echo "<b>Address : </b>";
+			echo $add,"<br>";
+			echo "<b>Phone : </b>";
+			echo $notel,"<br>";
+
+		?>
+	</body>
+</html>
